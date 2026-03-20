6230c70443377f6db49d30b6768e5535c34181b5
Guilherme-Caparroz <guiscaparroz@gmail.com>
2026-03-19 21:08:54 -0300

    Add files via upload

---
+++ Papagaio.java
@@ -0,0 +1,11 @@
+public class Papagaio {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Você não digitou nada");
+        } else {
+            System.out.println("Você digitou " + args[0]);
+        }
+
+        System.out.println("Criado por Guilherme Caparroz");
+    }
+}
